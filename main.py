4596f0ea97f9e5b8b25847d6683e8a428a0d5f48
DogeSec <utdoge@gmail.com>
2018-03-09 19:35:51 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-# Coming soon. lol
+import smtplib
+import sys
+
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls()
+server.login("your email here", "the pass of your email here") 
+    
+def mailer(email, message):
+    server.sendmail("Your email here", email, message)
+    server.quit()
+    print("Job done, program done.")
+mailer(sys.argv[1], sys.argv[2])
